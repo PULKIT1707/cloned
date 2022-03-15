63f00554bf448ac8c6e76c17b9db34eab8a4435c
Pulkit Srivastava <32233945+PULKIT1707@users.noreply.github.com>
2022-03-14 18:41:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
+          <h1 className="App-title">Demo App</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
+                  <Redirect to="/"/>
                 )}/>
                  <Route exact path='/customerlist' component={Customers} />
           </Switch>
